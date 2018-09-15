8a9b6d65076f0eb71c9ec0bd3a06ee003d2e8150
Khaled Othmane <khaled.othmane@gmail.com>
2018-09-15 14:51:59 +0100

    Initial Project

---
+++ src/main/java/com/khaledothmane/springpetclinicfs/SpringPetClinicFsApplication.java
@@ -0,0 +1,12 @@
+package com.khaledothmane.springpetclinicfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicFsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khaledothmane/springpetclinicfs/SpringPetClinicFsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khaledothmane.springpetclinicfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringPetClinicFsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
