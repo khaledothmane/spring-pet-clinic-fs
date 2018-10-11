66eb203ac59a4c19dd008349b7529fc96818db40
Khaled Othmane <khaled.othmane@gmail.com>
2018-10-11 19:50:59 +0100

    Show vets list in index page. Closes #15

--- pet-clinic-web/src/main/java/com/khaledothmane/spc/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/khaledothmane/spc/controllers/VetController.java
@@ -1,14 +1,24 @@
 package com.khaledothmane.spc.controllers;
 
+import com.khaledothmane.spc.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/vets"})
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"", "/", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
