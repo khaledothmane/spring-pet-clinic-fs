7200360863280ffc135b7a4854ce5289d52dc9f7
Khaled Othmane <khaled.othmane@gmail.com>
2018-10-22 21:26:30 +0100

    Create Speciality Service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/khaledothmane/spc/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.khaledothmane.spc.services;
+
+import com.khaledothmane.spc.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/khaledothmane/spc/services/map/SpecialityServiceMap.java
@@ -0,0 +1,34 @@
+package com.khaledothmane.spc.services.map;
+
+import com.khaledothmane.spc.model.Speciality;
+import com.khaledothmane.spc.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
