8ae0be258fe53cf6d1f654d608da261184ca734d
Khaled Othmane <khaled.othmane@gmail.com>
2018-11-12 18:27:38 +0000

    Create visit Entity. Closes #38

--- pet-clinic-data/src/main/java/com/khaledothmane/spc/model/Pet.java
+++ pet-clinic-data/src/main/java/com/khaledothmane/spc/model/Pet.java
@@ -1,11 +1,10 @@
 package com.khaledothmane.spc.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +20,9 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthday;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return Name;
@@ -52,5 +54,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
