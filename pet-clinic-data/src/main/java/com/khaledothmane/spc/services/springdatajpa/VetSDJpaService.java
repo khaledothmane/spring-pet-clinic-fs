df9331b849ea0673db4eb029e109cfeebf0c257a
Khaled Othmane <khaled.othmane@gmail.com>
2018-11-20 16:15:07 +0000

    Create Spring Data JPA Vet Service Closes #41

---
+++ pet-clinic-data/src/main/java/com/khaledothmane/spc/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.khaledothmane.spc.services.springdatajpa;
+
+import com.khaledothmane.spc.model.Vet;
+import com.khaledothmane.spc.repositories.VetRepository;
+import com.khaledothmane.spc.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().iterator().forEachRemaining(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Vet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
